fe3796f540f3b160b70a1e981daff3eeac8c337a
vijaynp10 <vnp350307@gmail.com>
2025-05-09 10:17:02 +0530

    Update Practical_6_3.cpp

--- Practical_6_3.cpp
+++ Practical_6_3.cpp
@@ -1,47 +1,69 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int n, m;
-    cout << "Enter size of the first and second array -> ";
-    cin >> n >> m;
-    int *arr1 = new int[n];
-    int *arr2 = new int[m];
-
-    cout << "Enter elements of arr 1-> " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Element [" << i << "] -> ";
-        cin >> arr1[i];
-    }
-
-    cout << "Enter elements of arr 2-> " << endl;
-    for (int i = 0; i < m; i++)
-    {
-        cout << "Element [" << i << "] -> ";
-        cin >> arr2[i];
-    }
+// Function to merge two sorted arrays into a new dynamically allocated array
+int* mergeSortedArrays(int* arr1, int size1, int* arr2, int size2, int& mergedSize) {
+    mergedSize = size1 + size2;
+    int* mergedArray = new int[mergedSize];
 
     int i = 0, j = 0, k = 0;
 
-    while (i < n && j < m)
-    {
-        if (arr1[i] < arr2[j])
-        {
-            merge[k++] = arr1[i++];
-        }
-        else
-        {
-            merge[k++] = arr2[j++];
+    // Merge while both arrays have elements
+    while (i < size1 && j < size2) {
+        if (arr1[i] < arr2[j]) {
+            mergedArray[k++] = arr1[i++];
+        } else {
+            mergedArray[k++] = arr2[j++];
         }
     }
-    int *merge = new int[n + m];
 
-    cout << "\nSorted & Merged Array is -> " << endl;
-    for (int i = 0; i < (n + m); i++)
-    {
-        cout << "Element [" << i << "] -> " << merge[i] << endl;
+    // Copy remaining elements from arr1
+    while (i < size1) {
+        mergedArray[k++] = arr1[i++];
     }
+
+    // Copy remaining elements from arr2
+    while (j < size2) {
+        mergedArray[k++] = arr2[j++];
+    }
+
+    return mergedArray;
+}
+
+int main() {
+    int size1, size2;
+
+    // Read sizes of both arrays
+    cout << "Enter size of first sorted array: ";
+    cin >> size1;
+    int* arr1 = new int[size1];
+    cout << "Enter elements of first sorted array: ";
+    for (int i = 0; i < size1; i++) {
+        cin >> arr1[i];
+    }
+
+    cout << "Enter size of second sorted array: ";
+    cin >> size2;
+    int* arr2 = new int[size2];
+    cout << "Enter elements of second sorted array: ";
+    for (int i = 0; i < size2; i++) {
+        cin >> arr2[i];
+    }
+
+    int mergedSize;
+    int* mergedArray = mergeSortedArrays(arr1, size1, arr2, size2, mergedSize);
+
+    // Display merged sorted array
+    cout << "Merged sorted array: ";
+    for (int i = 0; i < mergedSize; i++) {
+        cout << mergedArray[i] << " ";
+    }
+    cout << endl;
+
+    // Clean up dynamically allocated memory
+    delete[] arr1;
+    delete[] arr2;
+    delete[] mergedArray;
+
     return 0;
 }
